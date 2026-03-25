afcb4dd764e06ea7e9f0b1def18148d2759060d9
NEO101 <kunerodai@gmail.com>
2026-03-25 08:17:19 -0700

    Enhance TMC2209Driver bindings with parameters and methods
    Updated the TMC2209Driver binding to include constructor parameters and additional methods.

--- src/bindings.cpp
+++ src/bindings.cpp
@@ -1,3 +1,15 @@
+// #include <pybind11/pybind11.h>
+// #include "tmc2209_driver.h"
+
+// namespace py = pybind11;
+
+// PYBIND11_MODULE(tmc2209_module, m) {
+//     m.doc() = "TMC2209 Linux driver";
+//     py::class_<TMC2209Driver>(m, "TMC2209Driver")
+//         .def(py::init<>());
+// }
+// 03/25/2026
+
 #include <pybind11/pybind11.h>
 #include "tmc2209_driver.h"
 
@@ -6,5 +18,14 @@ namespace py = pybind11;
 PYBIND11_MODULE(tmc2209_module, m) {
     m.doc() = "TMC2209 Linux driver";
     py::class_<TMC2209Driver>(m, "TMC2209Driver")
-        .def(py::init<>());
+        .def(py::init<int, int, int, const char*>(),
+             py::arg("step_pin"),
+             py::arg("dir_pin"),
+             py::arg("en_pin"),
+             py::arg("uart_dev") = "/dev/ttyAMA2")
+        .def("configure", &TMC2209Driver::configure,
+             py::arg("current_ma"),
+             py::arg("microsteps"))
+        .def("set_enabled", &TMC2209Driver::setEnabled,
+             py::arg("en"));
 }
