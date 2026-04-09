e1a064ddc971ef2ec563dd58d209a7b292b1381b
NEO101 <kunerodai@gmail.com>
2026-04-09 10:22:55 -0700

    Implement runToPosition method for TMC2209Driver

--- src/tmc2209_driver.cpp
+++ src/tmc2209_driver.cpp
@@ -266,6 +266,46 @@ void TMC2209Driver::stepPulse(uint32_t steps, bool dir) {
     }
 }
 
+uint32_t TMC2209Driver::runToPosition(uint32_t steps, bool dir) {
+    if (steps == 0) return 0;
+
+    resetStall();
+    gpioWrite(dir_pin_, dir ? 1 : 0);
+
+    auto prof = computeProfile(steps, start_speed_, max_speed_, accel_);
+    uint32_t v = start_speed_;
+    uint32_t i = 0;
+
+    for (i = 0; i < steps; i++) {
+        if (isStalled()) {
+            break;
+        }
+
+        if (i < prof.accel_steps) {
+            v = start_speed_ + (i + 1) * accel_;
+            v = std::min(v, max_speed_);
+        } else if (i >= prof.accel_steps + prof.const_steps) {
+            uint32_t decel_i = i - prof.accel_steps - prof.const_steps;
+            uint32_t v_peak = start_speed_ + prof.accel_steps * accel_;
+            v_peak = std::min(v_peak, max_speed_);
+            v = (v_peak > (decel_i + 1) * accel_)
+                ? v_peak - (decel_i + 1) * accel_
+                : start_speed_;
+            v = std::max(v, start_speed_);
+        }
+
+        uint32_t half_us = 500000 / v;
+        if (half_us < 1) half_us = 1;
+
+        gpioWrite(step_pin_, 1);
+        gpioDelay(half_us);
+        gpioWrite(step_pin_, 0);
+        gpioDelay(half_us);
+    }
+
+    return i;
+}
+
 // --- Week 5: StallGuard ---
 
 void TMC2209Driver::setupStallGuard(uint8_t sgthrs, uint16_t tcoolthrs,
